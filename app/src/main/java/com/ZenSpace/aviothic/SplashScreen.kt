bf049e309230482cb7d2b0844bee16bb36a4a9de
Ayush Yadav <ayushyadav@Ayushs-MacBook-Air.local>
2023-05-11 23:49:34 +0530

    Splash Created

--- app/src/main/java/com/ZenSpace/aviothic/SplashScreen.kt
+++ app/src/main/java/com/ZenSpace/aviothic/SplashScreen.kt
@@ -1,12 +1,22 @@
 package com.ZenSpace.aviothic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.ImageView
+import android.os.Handler
 class SplashScreen : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            // Start the main activity after the splash screen is done
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2800)
+
     }
 }
