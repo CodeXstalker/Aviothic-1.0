b33b5e8cddac2cf5a609966e7e2628b4166de4ad
Shivansh Kumar <codes.stalker@gmail.com>
2023-05-12 02:15:17 +0530

    nothing

--- app/src/main/java/com/ZenSpace/aviothic/LoginActivity.kt
+++ app/src/main/java/com/ZenSpace/aviothic/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.ZenSpace.aviothic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var relative :RelativeLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
