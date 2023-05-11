1c4b462b1eb6c4d3ffc9f25d31abd864f8dd9a7c
Shivansh Kumar <codes.stalker@gmail.com>
2023-05-12 00:54:44 +0530

    Responsiveness Improved

--- app/src/main/java/com/ZenSpace/aviothic/SplashScreen.kt
+++ app/src/main/java/com/ZenSpace/aviothic/SplashScreen.kt
@@ -1,17 +1,24 @@
 package com.ZenSpace.aviothic
 
 import android.content.Intent
+import android.graphics.drawable.PictureDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.os.Handler
+import com.caverock.androidsvg.SVG
+
 class SplashScreen : AppCompatActivity() {
 
-
+    lateinit var logoIv: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        logoIv = findViewById(R.id.logo)
+
+
         Handler().postDelayed({
             // Start the main activity after the splash screen is done
             startActivity(Intent(this, MainActivity::class.java))
